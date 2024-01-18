050dfc62859c24ac10726a9a8ea8823416a5687e
유인호 <yuiop1029@naver.com>
2024-01-18 09:52:12 +0900

    실수에 대한 회고

--- SeSAC-Daily-17/Scene/Papago/ViewController/SelectLanguageViewController.swift
+++ SeSAC-Daily-17/Scene/Papago/ViewController/SelectLanguageViewController.swift
@@ -32,6 +32,8 @@ extension SelectLanguageViewController: UITableViewDelegate, UITableViewDataSour
 		cell.textLabel?.text = SelectLanguageViewController.languageDictionary[currentCase]
 
 		// 이런 부분에서 튜플로 짜놓은게 실수였던것 같습니다. Array로 했으면 좀 더 깔끔하게 처리할 수 있었을거 같아요.
+		// 동시에 두 값이 필요하지 않으니 애초에 처음부터 설계가 잘못된 거 같아요. 어떻게 바인딩 해줘야할지 감이 안와서 싱글톤 패턴으로 먼저 짜놓았던게 실수 같습니다.
+		// 튜플이 아니라 값을 하나만 받았다면 senderTag도 필요하지 않았겠죠.
 		if senderTag == 0 && currentCase == selectLang.inputLang {
 			cell.textLabel?.textColor = .red
 		} else if senderTag == 1 && currentCase == selectLang.outputLang {
